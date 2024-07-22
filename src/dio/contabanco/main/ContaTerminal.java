a35f9e9268d8768184e76009bdb0f4d64f262cc5
romulo guilherme <rgouveiaconsultoria@hotmail.com>
2024-07-22 19:11:16 -0300

    terminado o primeiro projeto bootcamp

--- src/dio/contabanco/main/ContaTerminal.java
+++ src/dio/contabanco/main/ContaTerminal.java
@@ -1,7 +1,25 @@
 package dio.contabanco.main;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Olá! Nos diga seu primeiro nome : ");
+        String nome = scan.next();
+        System.out.println("Nos diga seu segundo nome: ");
+        String segNome = scan.next();
+
+        System.out.println("Qual numero da sua conta? ");
+        int numeroConta = scan.nextInt();
+
+        System.out.println("Qual é a sua agencia? ");
+        String agencia = scan.next();
+
+        System.out.println("Seu saldo: ");
+        float saldo = scan.nextFloat();
+
+        System.out.println("Olá, "+ nome.concat(" ").concat(segNome) + "! obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta "+ numeroConta + " e seu saldo " + saldo +" já está disponível para saque");
     }
 }
